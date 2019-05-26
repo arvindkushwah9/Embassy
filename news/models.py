20e1f991019bbd10274e183249c8f208ee1ef3dd
Amal <a.m.a.azazi@gmail.com>
2019-05-26 15:23:40 +0800

    Create post model and migration

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title= models.CharField(max_length=200)
+    description= models.TextField()
+    content= models.TextField()
+    image= models.ImageField()
+    pub_date= models.DateTimeField('date published')
+    update_date= models.DateTimeField('date updated')
+    creator= models.ForeignKey(User, related_name="id+", on_delete=models.DO_NOTHING)
+    updater= models.ForeignKey(User, related_name="id+", on_delete=models.DO_NOTHING)
