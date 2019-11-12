3980303259e4123df6acb2341d0064dc09d16713
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-11-12 19:45:35 +0530

    Added pg config

--- esa_backend/settings.py
+++ esa_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,17 +78,19 @@ WSGI_APPLICATION = 'esa_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'esa_backend',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        # 'HOST': '127.0.0.1',
-        'PORT': 3306,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'esa_backend',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         # 'HOST': '127.0.0.1',
+#         'PORT': 3306,
+#     }
+# }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
