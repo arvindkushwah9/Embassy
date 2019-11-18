778fd7023a79568d7bd3b8f7de8be4feed79e928
Arvind kushwah <arvindkushwah9@gmail.com>
2019-11-18 19:38:46 +0530

    Fix text area

--- sendemail/forms.py
+++ sendemail/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 21}), required=True)
