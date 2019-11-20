d7662ba05e565ae897a2d85b456e1ac4745f58b6
Arvind kushwah <arvindkushwah9@gmail.com>
2019-11-20 19:38:27 +0530

    Add crud on notification

---
+++ notifications/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('new', views.new, name='new'),
+    
+
+    path('show',views.show), 
+
+    #ex: /notifications/5/edit/
+    path('edit/<int:id>', views.edit),
+     # ex: /notifications/5/update/
+    path('update/<int:id>', views.update), 
+    path('delete/<int:id>', views.destroy),
+]
