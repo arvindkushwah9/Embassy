f33616f414c5e704ab902f1014bc67efc4dddbd6
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-12-02 14:05:32 +0530

    Added prifile in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from accounts.models import UserProfile
+from django.utils.html import format_html
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+  readonly_fields = ('id',)
+  list_display = ('user','passport_number', 'phone_number')   
+  
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
