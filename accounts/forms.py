6e133e3f45cf2882c372334c5d7ec2dc4692a504
Arvind kushwah <arvindkushwah9@gmail.com>
2019-12-17 18:17:24 +0530

    Changes type password on Signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,11 +5,10 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     passport_number = forms.CharField(max_length=30, required=False, help_text='Optional.', label="Passport Number")
-    phone_number = forms.CharField(max_length=30, required=False, help_text='Optional.', label="Phone Number")
+    phone_number = forms.IntegerField(required=True, help_text='Optional.', label="Phone Number")
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    phone_number = forms.CharField()
-    password1 = forms.CharField()
-    password2 = forms.CharField()
+    password1 = forms.PasswordInput()
+    password2 = forms.PasswordInput()
 
     class Meta:
         model = User
@@ -18,8 +17,8 @@ class SignUpForm(UserCreationForm):
 class UpdateProfile(forms.ModelForm):
     username = forms.CharField(required=True)
     email = forms.EmailField(required=True)
-    passport_number = forms.CharField(required=False)
-    phone_number = forms.CharField(required=False)
+    passport_number = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)
 
     class Meta:
         model = User
